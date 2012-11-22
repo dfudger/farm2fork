eab492ee7c6c3d643a2a3c1335a5880757c01c0d
Matthew Roberts <mrober13@uoguelph.ca>
2012-11-22 13:00:47 -0500

    fixing dev environment

--- farm2fork_otmd/__init__.py
+++ farm2fork_otmd/__init__.py
@@ -20,7 +20,7 @@ def main(global_config, **settings):
     authz_policy = ACLAuthorizationPolicy()
     #Base.metadata.bind = engine
     config = Configurator(settings=settings,
-                          root_factory='farmtofork.models.RootFactory')
+                          root_factory='.models.RootFactory')
     config.set_authentication_policy(authn_policy)
     config.set_authorization_policy(authz_policy)
     config.include('pyramid_celery')
